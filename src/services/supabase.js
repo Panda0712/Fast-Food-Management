9dd4e73bcaa30dcce1bde2803e0432b09dfe798d
Pham Tuan <tuan.pham.super@gmail.com>
2025-11-22 13:54:47 +0700

    update supabase url and api key for new restored database

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
-export const supabaseUrl = "https://iizqqbdczorzypgneais.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlpenFxYmRjem9yenlwZ25lYWlzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjYzNzM0MDMsImV4cCI6MjA0MTk0OTQwM30.9oRoFnOjJapMUTPI19Q6UP1_XQvRm-TyL7u57Q_2x6s";
+export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
